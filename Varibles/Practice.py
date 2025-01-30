4c983e972197b9e9984a100d21396c8866055bcf
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-01-30 11:13:28 -0700

    save practice

--- Varibles/Practice.py
+++ Varibles/Practice.py
@@ -2,6 +2,8 @@
 
 name = input("What is your name: ")
 
+small_number = input("How old are you: ")
+
 school = input("Where do you go to for an education: ")
 
 subject = input("What is your favorite subject: ")
@@ -10,7 +12,9 @@ subject = input("What is your favorite subject: ")
 
 color = input("What is your favorite color: ")
 
+food = input("What did you eat for breakfast: ")
 
+eyes = input("What color are your eyes: ")
 
 print(name, "really likes", subject)
 

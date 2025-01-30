e799227f46ca42cadfb5b41e86520ca1d50b28a7
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-01-30 11:49:55 -0700

    save template

--- template.c
+++ template.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+char name[] = "Evelyn";
+int age = 15;
+float pi = 3.14;
+
 int main(void){
-    printf("Hello World");
+    printf("Hello I am %s. I am %d years old. I like the number %f.\n", name);
+    printf("%d\n", age);
+    printf("%f\n", pi);
     return 0;
 }

b5a710df2debe11ff101a61edb4d7425c42baa04
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-01-31 11:48:37 -0700

    save

--- practice.c
+++ practice.c
@@ -5,7 +5,7 @@ int age = 15;
 float pi = 15.25;
 
 int main(void){
-    printf("Hello I am %s. I am %d years old. I am exactly %f years old.\n", name);
+    printf("Hello I am %s. I am %d years old. I am exactly %f years old.\n", name, age, pi);
     printf("%d\n", age);
     printf("%f\n", pi);
     return 0;

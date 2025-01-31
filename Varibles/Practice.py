5b8a810d2de68e17fe288e628034d5d46fdc142b
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-01-31 11:13:05 -0700

    save practice.py

--- Varibles/Practice.py
+++ Varibles/Practice.py
@@ -2,13 +2,15 @@
 
 name = input("What is your name: ")
 
-small_number = input("How old are you: ")
+age = input("How old are you: ")
 
 school = input("Where do you go to for an education: ")
 
+small_number = input("How many classes do you have: ")
+
 subject = input("What is your favorite subject: ")
 
-
+year = input("What is the year: ")
 
 color = input("What is your favorite color: ")
 
@@ -16,6 +18,12 @@ food = input("What did you eat for breakfast: ")
 
 eyes = input("What color are your eyes: ")
 
-print(name, "really likes", subject)
+random = input("How many days are in a year: ")
 
-name = "Alex"
+print(name, "is", age, "old.")
+print("They go to", school, "and have", small_number, "classes.")
+print("Their favorite subject in", school, "is", subject, ".")
+print("The year is", year, "and there are", random, "days in a year.")
+print(name, "'s eyes are", eyes, ".")
+print("They ate", food, "this morning.")
+print ("Their favorite color", color, ".")

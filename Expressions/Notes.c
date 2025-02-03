65e7e3d9f247ec33d5468cd44b6a2166d8c3ca6b
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-03 11:49:25 -0700

    save

---
+++ Expressions/Notes.c
@@ -0,0 +1,10 @@
+// Evelyn Chennault, Expression Notes
+#include <stdio.h>\
+#include <math.h>
+float x = pow(2,2);
+float equation = 5*7+11/(5-pow(2,2));
+// + - * /
+int main(void){
+    //printf("%f", equation);
+    return 0;
+}

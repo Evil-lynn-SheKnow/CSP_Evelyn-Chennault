12985c2421655138d81f430052438d83fea3760b
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-05 11:12:48 -0700

    save

--- Expressions/Notes.c
+++ Expressions/Notes.c
@@ -3,12 +3,12 @@
 #include <math.h>
 int num = 12;
 float pi = 3.1415926535;
+float expression = 5*pow(7, 2)/4;
 // + - * /
 int main(void){
     num = 4;
     //printf("%d\n", num);
     //printf("%f", equation);
-
-    printf("%.8f", pi);
+    //printf((int)6+9/2);
     return 0;
 }

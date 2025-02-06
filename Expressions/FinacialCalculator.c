70b6f9eeaf53d17fef9fbc3a4c75d4842863c044
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-06 11:47:36 -0700

    svae changes and created financial calculator

---
+++ Expressions/FinacialCalculator.c
@@ -0,0 +1,13 @@
+// Evelyn Chennault, Finacial Calculator C
+#include <stdio.h>
+
+char name[] = "Evelyn";
+int age = 15;
+float pi = 3.14;
+
+int main(void){
+    printf("Hello I am %s. I am %d years old. I like the number %f.\n", name);
+    printf("%d\n", age);
+    printf("%f\n", pi);
+    return 0;
+}

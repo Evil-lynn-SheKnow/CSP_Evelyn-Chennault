bccbd81f17997bd3cf3b754d7dff510fd27e619b
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-09 03:08:15 +0000

    save edits to finacial calculator in c

--- Expressions/FinacialCalculator.c
+++ Expressions/FinacialCalculator.c
@@ -1,13 +1,34 @@
 // Evelyn Chennault, Finacial Calculator C
 #include <stdio.h>
 
-char name[] = "Evelyn";
-int age = 15;
-float pi = 3.14;
+// Tells user what the program does
+printf("Hello! I am a finacial calculator. Let's begin!");
+printf("I will ask you several questions. Please answer them in terms of US dollars.");
+
+// Questions for the user to fill out in order for the calculator to get the varibles
+float income = printf("What is your income? \n");
+float rent = printf("What is your rent? \n");
+float utilities = printf("How much do you spend on utilities? \n");
+float groceries = printf("How much do you spend on groceries? \n");
+float transportation = printf("How much do you spend on transportation? \n");
+float savings = printf("How much is in your savings account? \n");
+float spendings = printf("How much are you usually spending? \n");
+
+// Ten percent of income
+float incomePercent = (income*.1)
+
+// Money left over after spendings and current savings
+float 
+
+// Math equations that are needed to calculate the percentages
+
 
 int main(void){
-    printf("Hello I am %s. I am %d years old. I like the number %f.\n", name);
-    printf("%d\n", age);
-    printf("%f\n", pi);
+
+    printf("Your rent is %f, which is THIS PERCENT of your income.\n", income);
+    printf("Your spendings on utilities is %f, which is THIS PERCENT of your income.\n", utilities);
+    printf("Your spendings on groceries is %f, which is THIS PERCENT of your income.\n", groceries);
+    printf("Your spendings on transportation is %f, which is THIS PERCENT of your income.\n", transportation);
+
     return 0;
 }

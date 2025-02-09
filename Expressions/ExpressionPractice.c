6470666d6c0f44b849d355828aab6a3dd6473b38
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-09 03:31:47 +0000

    changed expression practice

--- Expressions/ExpressionPractice.c
+++ Expressions/ExpressionPractice.c
@@ -1,13 +1,9 @@
 //Evelyn Chennault, Expressions Practice C
 #include <stdio.h>
 
-char name[] = "Evelyn";
-int age = 15;
-float pi = 3.14;
+
 
 int main(void){
-    printf("Hello I am %s. I am %d years old. I like the number %f.\n", name);
-    printf("%d\n", age);
-    printf("%f\n", pi);
+
     return 0;
 }

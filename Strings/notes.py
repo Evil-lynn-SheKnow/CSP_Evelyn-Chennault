27e2bd850804fc9574ff23f8252f5898c38b2c45
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-10 11:48:59 -0700

    save notes

---
+++ Strings/notes.py
@@ -0,0 +1,13 @@
+# Evelyn Chnenault, Strings Notes Python
+
+# strings are a data type that holds any info surrounded by quotation marks "" ''
+
+note = "Ms. LaRose's class"
+
+name = input("What is your first name?\n")
+
+# print(f"Hello, {name}. Welcome to my program!").strip().capitalize()
+
+print("this is your name: "+ name)
+
+sentence = "The quick brown fox jumps over the lazy dog"

a94d5ca11e385735bbef76c64dc39659a0cdd6c5
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-11 11:43:23 -0700

    save notes

--- Strings/notes.c
+++ Strings/notes.c
@@ -1,12 +1,34 @@
 // Evelyn Chennault, Notes C
 #include <stdio.h>
+#include <string.h>
 
 char subject[50];
+char name[] = "Victoria";
+char sentence[] = "The quick brown fox jumps over the lazy dog";
 
 int main(void){
     //printf("What class are you in?\n");
     //scanf("%s", subject);
-    fgets(subject, sizeof(subject), stdin);
-    printf("You are in %s, that is a cool class.");
+    //fgets(subject, sizeof(subject), stdin);
+    //printf("You are in %s, that is a cool class.");
+    //name[0] = 'T';
+    //name[1] = 'o';
+    //name[2] = 'r';
+    //name[3] = 'i';
+    //name[4] = ' ';
+    //name[5] = ' ';
+    //name[6] = ' '; 
+    //name[7] = ' ';
+    //printf("%s", name);
+    //printf("%lu\n", sizeof(sentence));
+    //printf("%d\n", strlen(sentence));
+    char one[] = "Hello ";
+    char two[] = "World!";
+    char three[] = "Welcome to my program. ";
+    printf("%s\n", one);
+    strcat(one, two);
+    printf("%s\n", one);
+    strcat(three, one);
+    printf("%s\n", three);
     return 0;
 }

cb65faa081ecdea26de1ce1706e6e03d6f2d2a49
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-11 11:15:27 -0700

    python string notes

--- Strings/notes.py
+++ Strings/notes.py
@@ -2,12 +2,18 @@
 
 # strings are a data type that holds any info surrounded by quotation marks "" ''
 
-note = "Ms. LaRose's class"
+# note = "Ms. LaRose's class"
 
-name = input("What is your first name?\n")
+# name = input("What is your first name?\n")
 
 # print(f"Hello, {name}. Welcome to my program!").strip().capitalize()
 
-print("this is your name: "+ name)
+# print("This is your name: "+ name)
 
 sentence = "The quick brown fox jumps over the lazy dog"
+
+# print(len(sentence))
+# print(sentence[4])
+start = sentence.find("brown")
+length = len("brown fox")
+print(sentence[start:start+length])

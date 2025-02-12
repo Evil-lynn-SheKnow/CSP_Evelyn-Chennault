afc79ddd3314ffd4ed8070fb4b0ac86d23087b97
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-12 11:29:43 -0700

    most of name dec finished. Work in cat

--- Strings/name_decorator.py
+++ Strings/name_decorator.py
@@ -1,2 +1,16 @@
 # Evelyn Chnenault, Name Decorator Python
 
+print("Hello, user! Welcome to the name decorator.")
+name = input("What is your name?\n").strip().lower()
+
+print("<<<", name, ">>>")
+
+nameTwo = input("Would you like another version of your name decorated? If yes, type you name. If no, leave the program.\n").strip().upper()
+
+print("<--", nameTwo, "-<<")
+
+nameThree = input("Would you like another version of your name decorated? If yes, type you name. If no, leave the program.\n").strip().upper()
+
+print("(-_-)", nameThree, "(^.^)")
+
+print("Thank you for using this program! I hope to see you again! Bye bye!")

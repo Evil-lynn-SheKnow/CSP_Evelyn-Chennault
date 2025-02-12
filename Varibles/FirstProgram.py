1c064496832a79c9e0754031bf61be4b0e43d078
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-12 04:13:17 +0000

    save program

--- Varibles/FirstProgram.py
+++ Varibles/FirstProgram.py
@@ -1,7 +1,7 @@
 # Evelyn Chennault, First Program Python
 
-name = print("Hello. What is your name: ")
+name = input("Hello. What is your name: ")
 
-subject = print("What is your favorite school subject: ")
+subject = input("What is your favorite school subject: ")
 
 print("My name is", name, "and my favorite subject at school is", subject)

b0e8cbd6cbf6af69e427144c3c25d81dfe92a2c2
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-14 11:15:44 -0700

    save changes

--- Strings/silly_sentences.c
+++ Strings/silly_sentences.c
@@ -29,9 +29,9 @@ int main(void){
     scanf("%s", &object2);
     printf("Type in a emotion:\n");
     scanf("%s", &emotion);
+    //Print madlib
     printf("I was in my %s when my %s had %s me over to his room.", place, person, verb);
     printf("I when into my %s's room and he said to help him %s John, our cousin.", person, verb2);
     printf("Me and my %s got the %s and the %s. We went to the %s next door and %s the room. My brother was not %s.", person, object, object2, place2, emotion);
-
     return 0;
 }

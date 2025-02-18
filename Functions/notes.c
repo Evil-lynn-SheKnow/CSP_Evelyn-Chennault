1c2560655c5f777c90adf6dfa0f815ce944c08b9
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-18 11:47:10 -0700

    save changes

--- Functions/notes.c
+++ Functions/notes.c
@@ -1,8 +1,22 @@
 //Evelyn Chennault, Functions Notes C
 #include <stdio.h>
 
+int num;
+char name[50], place[50], verb[50];
+
+void add(int numOne, int numTwo){
+    return numOne+numTwo;
+}
+
+const char* word(type){
+    
+}
 
 int main(void){
-    printf("Hello Wolrd");
+    //printf("Please tell me a number:\n");
+    //scanf("%d", num);
+    //add(num,10);
+    //add(1,8);
+    //printf (add(72,5));
     return 0;
 }

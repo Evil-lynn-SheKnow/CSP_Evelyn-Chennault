44a2a484077a77689917f45598a8168be2f1cf43
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-19 05:08:59 +0000

    save

--- Strings/name_decorator.c
+++ Strings/name_decorator.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 
 char name;
-char nameTwo;
-char nameThree;
 
 int main(void){
-    printf("Please type in your name: \n");
+    printf("Please type in your name:\n");
     scanf("%s", &name);
     printf("<<< %s >>>");
    

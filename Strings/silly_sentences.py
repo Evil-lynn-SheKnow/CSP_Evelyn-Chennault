c29e19df97248eb417cd4f37af0a4c1f170da1d3
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-19 04:58:14 +0000

    save changes and silly sentences

---
+++ Strings/silly_sentences.py
@@ -0,0 +1,20 @@
+# Evelyn Chennault, Silly Sentences Python
+
+print("Hello, user! Welcome to the madlib program.")
+
+noun = input("Please give me a person's name:\n").strip().capitalize()
+
+noun_2 = input("Please give me another noun:\n").strip().lower
+
+noun_3 = input("I'm sorry--give me another noun:\n").strip().lower
+
+noun_4 = input("Last noun:\n").strip().lower
+
+verb = input("New thing! Please give me a verb in PAST TENSE:\n").strip().lower
+
+
+print(noun, "was", verb, "to the", noun_2, "to get some", noun_3, "for his", noun_4, ", who was at the house.\n")
+
+print("Thank you for using this program!")
+print("I hope to see you again!")
+print("Bye bye! =)")

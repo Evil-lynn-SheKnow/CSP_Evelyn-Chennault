d7ff008a5e2d4e9b190555e0c229e19aa8d82aaa
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-19 11:49:15 -0700

    save

--- Strings/name_decorator.c
+++ Strings/name_decorator.c
@@ -1,12 +1,14 @@
 // Evelyn Chennault, Name Decorator C
 #include <stdio.h>
-
-char name;
+#include <string.h>
+#include <ctype.h>
 
 int main(void){
+    char name[50];
+    printf("This is the name decorator.\n");
     printf("Please type in your name:\n");
-    scanf("%s", &name);
-    printf("<<<%s>>>");
+    scanf("%s", toupper(name));
+    printf(">>-%s-->\n", name);
    
     return 0;
 }

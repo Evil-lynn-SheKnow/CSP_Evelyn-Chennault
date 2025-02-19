9805fdba28a2e056d907cbc651e81f7127f22f83
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-19 11:19:39 -0700

    edited python decorator

--- Strings/name_decorator.py
+++ Strings/name_decorator.py
@@ -1,15 +1,18 @@
 # Evelyn Chnenault, Name Decorator Python
 
+str_one = "Would you like another version of your name decorated?"
+str_two = "If yes, type you name. If no, leave the program."
+
 print("Hello, user! Welcome to the name decorator.")
 name = input("What is your name?\n").strip().lower()
 
 print("<<<", name, ">>>\n")
 
-nameTwo = input("Would you like another version of your name decorated? If yes, type you name. If no, leave the program.\n").strip().upper()
+nameTwo = input(f"{str_one} {str_two}\n").strip().upper()
 
 print("<--", nameTwo, "-<<\n")
 
-nameThree = input("Would you like another version of your name decorated? If yes, type you name. If no, leave the program.\n").strip().upper()
+nameThree = input(f"{str_one} {str_two}\n").strip().upper()
 
 print("(-_-)", nameThree, "(^.^)\n")
 

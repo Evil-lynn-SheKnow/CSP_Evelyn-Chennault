55ff54069450c8a89407df6aa6bf454683493a04
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-20 05:05:15 +0000

    save all changes, modified to correct expectations

--- Expressions/FinacialCalculator.py
+++ Expressions/FinacialCalculator.py
@@ -1,56 +1,67 @@
 # Evelyn Chennault, Financial Calculator Python
 
 # print satement that welcomes my user and tells what the program does
-print("Hello, user. I am a finacial calculator. I will calculate your spendings and savings. Let's begin!")
+print("Hello, user. I am a finacial calculator. I will calculate your spendings and savings. Let's begin!\n")
+
 
 # ask what their income is (vaible an input)
-income = float(input("What is your income? \n"))
+income = float(input("What is your income?\n"))
 # ask what their rent is (vaible an input)
-rent = float(input("What is your rent? \n"))
+rent = float(input("What is your rent?\n"))
 # ask what their utilies is (vaible an input)
-utilites = float(input("How much money do you pay for utilites? \n"))
+utilites = float(input("How much money do you pay for utilites?\n"))
 # ask what their groceries is (vaible an input)
-groceries = float(input("How much money do you pay for groceries? \n"))
+groceries = float(input("How much money do you pay for groceries?\n"))
 # ask what their transportation is (vaible an input)
-transportation = float(input("How much money do you pay for transportation? \n"))
-# ask what their spendings are (vaible an input)
-spendings = float(input("How much money do you pay for everything? \n"))
+transportation = float(input("How much money do you pay for transportation?\n"))
 # ask what their savings are (vaible an input)
-savings = float(input("How much money do you pay save? \n"))
+savings = float(input("How much money do you pay save?\n"))
+
+print("\n")
 
 # calculate savings as 10% of income (income*.1) (varible)
-incomePercent = (income*.1)
+income_percent = (income*.1)
 # calculate spending as income-savings-rent-utilites-groceries-transportation
-leftOverMoney = (income-savings-rent-utilites-groceries-transportation)
+left_over_money = (income-savings-rent-utilites-groceries-transportation)
+# calculate what their spendings are (vaible an input)
+spendings = (((rent+utilites+groceries+transportation)/income)*100)
+
 
 # calculate percent income of rent (rent/income*100) (varible)
-rentPercent = ((rent/income)*100)
+rent_percent = ((rent/income)*100)
 # calculate percent income of utilites (utilites/income*100) (varible)
-utilitesPercent = ((utilites/income)*100)
+utilites_percent = ((utilites/income)*100)
 # calculate percent income of groceries (groceries/income*100) (varible)
-groceriesPercent = ((groceries/income)*100)
+groceries_percent = ((groceries/income)*100)
 # calculate percent income of transportation (transportation/income*100) (varible)
-transportationPercent = ((transportation/income)*100)
+transportation_percent = ((transportation/income)*100)
 # calculate percent income of spending (savings/income*100) (varible)
-savingsPercent = ((savings/income)*100)
+savings_percent = ((savings/income)*100)
 # calculate percent income of spending (spendings/income*100) (varible)
-spendingsPercent = ((spendings/income)*100)
+spendings_percent = ((spendings/income)*100)
+# calculate spending as income-savings-rent-utilites-groceries-transportation
+left_over_money_percent = ((left_over_money/income)*100)
+
 
 # ten percent of your income
-print(incomePercent, "is 10 percent of your income.")
+print(income_percent, "is 10 percent of your income.")
 # money left over after all the monthly spending and saving
-print(leftOverMoney, "is 10 percent f your income.")
+print(left_over_money, "which is", left_over_money_percent, "percent of your income.")
 # your rent is $XX.XX which is XX% of your income. (print)
-print("Your rent is", rent, "which is", rentPercent, "percent of your income.")
+print("Your rent is", rent, "which is", rent_percent, "percent of your income.")
 # your utilities is $XX.XX which is XX% of your income. (print)
-print("Your utilies are", utilites, "which is", utilitesPercent, "percent of your income.")
+print("Your utilies are", utilites, "which is", utilites_percent, "percent of your income.")
 # your groceries is $XX.XX which is XX% of your income. (print)
-print("Your groceries are", groceries, "which is", groceriesPercent, "percent of your income.")
+print("Your groceries are", groceries, "which is", groceries_percent, "percent of your income.")
 # your transportation is $XX.XX which is XX% of your income. (print)print("Your rent is", rent, "which is", incomePercent, "of your income.")
-print("Your transportation is", transportation, "which is", transportationPercent, "percent of your income.")
+print("Your transportation is", transportation, "which is", transportation_percent, "percent of your income.")
 # your savings is $XX.XX which is XX% of your income. (print)
-print("Your savings are", savings, "which is", savingsPercent, "percent of your income.")
+print("Your savings are", savings, "which is", savings_percent, "percent of your income.")
 # your spendings is $XX.XX which is XX% of your income. (print)
-print("Your spendings are", spendings, "which is", spendingsPercent, "percent of your income.")
+print("Your spendings are", spendings, "which is", spendings_percent, "percent of your income.")
+# any left over money is $XX.XX which is XX% of your income. (print)
+print("You have", left_over_money, "which is", left_over_money_percent, "percent of your income.")
+
+print("\n")
 
 print("Thank you for trying me out! I hope I helped with your finacial needs.")

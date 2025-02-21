b822f5341628cf53d3c85caa9b7c07a81a65adc5
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-21 11:31:26 -0700

    save changes

--- Strings/name_decorator.py
+++ Strings/name_decorator.py
@@ -1,8 +1,7 @@
 # Evelyn Chnenault, Name Decorator Python
 
 str_one = "Would you like another version of your name decorated?"
-str_two = "If yes, type you name. If no, leave the program."
-name_four = name.strip().capitalize()
+str_two = "Just type your name in again!"
 
 print("Hello, user! Welcome to the name decorator.")
 name = input("What is your name?\n").strip().lower()
@@ -17,6 +16,7 @@ name_three = input(f"{str_one} {str_two}\n").strip().upper()
 
 print("(-_-)", name_three, "(^.^)\n")
 
-print("Thank you for using this program, " + name_four)
-print("I hope to see you again!")
+
+name_four = name.strip().capitalize()
+print("Thank you for using this program, " + name_four + ".")
 print("Bye bye!")

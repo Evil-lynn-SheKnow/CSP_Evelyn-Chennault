db6807f17baf97373b306704c31c98e6c0ab195b
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-22 23:55:16 +0000

    save changes

--- Functions/FinacialCalculatorUpdate.py
+++ Functions/FinacialCalculatorUpdate.py
@@ -5,45 +5,28 @@ print("Hello, user. I am a finacial calculator. I will calculate your spendings 
 
 # user inputs
 income = float(input("What is your income?\n"))
-rent = float(input("How much money do you pay for rent?\n"))
-utilites = float(input("How much money do you pay for utilites?\n"))
-groceries = float(input("How much money do you pay for groceries?\n"))
-transportation = float(input("How much money do you pay for transportation?\n"))
-savings = float(income-utilites-groceries-transportation)
-spendings = float(utilites+groceries+transportation)
 
+def info(type):
+    return input(f"How much do you have to pay for {type}:\n")
 
-def info(cost, income, type):
-    percent1 = rent/income *100
-    print(f"Your {type} is {cost:.2} which is {percent1}% of your income.")
-
-def info_two(cost, income, type):
-    percent2 = utilites/income *100
-    print(f"Your {type} is {cost:.2} which is {percent2}% of your income.")
-
-def info_three(cost, income, type):
-    percent3 = groceries/income *100
-    print(f"Your {type} is {cost:.2} which is {percent3}% of your income.")
-
-def info_four(cost, income, type):
-    percent4 = transportation/income *100
-    print(f"Your {type} is {cost:.2} which is {percent4}% of your income.")
-
-def info_five(cost, income, type):
-    percent5 = savings/income *100
-    print(f"Your {type} is {cost:.2} which is {percent5}% of your income.")
-
-def info_six(cost, income, type):
-    percent6 = spendings/income *100
-    print(f"Your {type} is {cost:.2} which is {percent6}% of your income.")
-
-
-income = info("income"), info_two("income"), info_three("income"), info_four("income"), info_five("income"), info_six("income")
 rent = info("rent")
-utilites = info_two("utilites")
-groceries = info_three("groceries")
-transportation = info_four("transportation")
-savings = info_five("savings")
-spendings = info_six("spendings")
+utilites = info("utilites")
+groceries = info("groceries")
+transportation = info("transportation")
+
+rent_per = (round(rent/income*100))
+utilites_per = utilites/income*100
+groceries_per = groceries/income*100
+transport_per = transportation/income*100
+savings = income*.1
+spendings = income-rent-utilites-groceries-transportation-savings
+
+print(f"Your is ${income}.")
+print(f"You spend ${rent} for your rent, which is {rent_per}% of your income.")
+print(f"You spend ${utilites} for your utilites, which is {utilites_per}% of your income.")
+print(f"You spend ${groceries} for your groceries, which is {groceries_per}% of your income.")
+print(f"You spend ${transportation} for transportation, which is {transport_per}% of your income.")
+print(f"You spend ${spendings} for all purposes,.")
+print(f"${savings} is 10% of your income.")
 
 print("Thank you for trying me out! I hope I helped with your finacial needs.")

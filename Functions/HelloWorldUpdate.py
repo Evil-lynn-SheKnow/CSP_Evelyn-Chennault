3da9a5a91bc3f6e2b0c146a16d0bec89bd66444c
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-22 22:32:38 +0000

    save changes to actual update of hello world

--- Functions/HelloWorldUpdate.py
+++ Functions/HelloWorldUpdate.py
@@ -1,8 +1,19 @@
 #Evelyn Chennault - Hello World Update Python
 
+print("Hello, user. Teach me some fun names! Please give me five!")
+
 def hello(name):
-    return input("What is your name?\n").strip().capitalize()
+    return input(f"Please give me a {name}\n").strip().capitalize()
     
 name = hello("name")
+name_two = hello("name")
+name_three = hello("name")
+name_four = hello("name")
+name_five = hello("name")
 
-print(f"Your name is {name}. Hello, {name}!")
+print(f"A cool name is {name}.")
+print(f"A funny name is {name_two}.")
+print(f"A scary name is {name_three}.")
+print(f"A cute name is {name_four}.")
+print(f"A normal name is {name_five}.")
+print(f"I like the name {name_five}. I am going to call you {name_five}. Hello, {name}!")

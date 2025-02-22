3d57c74ec93ab7eb1792970a24d703fa6739c411
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-02-22 03:50:05 +0000

    silly sentences in python changed properly

--- Strings/silly_sentences.py
+++ Strings/silly_sentences.py
@@ -4,16 +4,17 @@ print("Hello, user! Welcome to the madlib program.")
 
 noun = input("Please give me a person's name:\n").strip().capitalize()
 
-noun_2 = input("Please give me another noun:\n").strip().lower
+noun_2 = input("Please give me noun:\n").strip().lower()
 
-noun_3 = input("I'm sorry--give me another noun:\n").strip().lower
+noun_3 = input("Give me another noun:\n").strip().lower()
 
-noun_4 = input("Last noun:\n").strip().lower
+noun_4 = input("Last noun:\n").strip().lower()
 
-verb = input("New thing! Please give me a verb in PAST TENSE:\n").strip().lower
+verb = input("New thing! Please give me a verb in PRESENT TENSE:\n").strip().lower()
 
+print("\n")
 
-print(noun, "was", verb, "to the", noun_2, "to get some", noun_3, "for his", noun_4, ", who was at the house.\n")
+print(noun + " was " + verb + " to the " + noun_2 + " to get some " + noun_3 + " for his " + noun_4 + ", who was at the house.\n")
 
 print("Thank you for using this program!")
 print("I hope to see you again!")

a9f84a25b516266f5e371ff9bb63184db881d2ff
CSP_Evelyn-Chennault <evelyn.chennault@ucas-edu.net>
2025-02-28 11:24:49 -0700

    save changes and time of day

--- Conditionals/TimeOfDay.c
+++ Conditionals/TimeOfDay.c
@@ -3,24 +3,29 @@
 #include <time.h>
 
 int main(void){
-    time_t seconds;
-    seconds = time(NULL);
-    printf("Seocnds since January 1, 1970 = %d\n", seconds);
-
-    //Current time
-    time_t rawtime;
-    struct tm * timeinfo;
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    //Current time  
+    time_t rawtime;  
+    struct tm * timeinfo;  
+    time(&rawtime);  
+    timeinfo = localtime(&rawtime);  
     printf("Current time and date is %s\n", asctime(timeinfo));
+    //Current hour  
+    time_t now = time(NULL);  
+    //now = time(NULL); //same thing  
+    struct tm * tm_struct = localtime(&now);  
+    int hour = tm_struct->tm_hour; //military time  
+    printf("In military time, the time is %d.", hour);
+    //Greet user based off of hour
+    if(hour == 6 || hour == 7 || hour == 8 || hour == 9 || hour == 10 || hour == 11){
+        printf("Good morning!");
+    }else if(hour == 12 || hour == 13 || hour == 14 || hour == 15 || hour == 16){
+        printf("Good afternoon!");
+    }else if(hour == 17 || hour == 18 || hour == 19 || hour == 20 || hour == 21 || hour == 22){
+        printf("Good evening!");
+    }else if(hour == 23 || hour == 0 || hour == 1 || hour == 2 || hour == 3 || hour == 4 || hour == 5){
+        printf("Good night!");
+    }
 
-    //Current hour
-    time_t now = time(NULL);
-    //now = time(NULL); //same thing
-    struct tm * tm_struct = localtime(&now);
-    int hour = tm_struct->tm_hour; //military time
-    printf("%d\n", hour);
+return 0;  
 
-    return 0;
 }

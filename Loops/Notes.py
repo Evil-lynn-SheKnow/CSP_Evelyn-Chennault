609dab13856e026d281d5f580aee7645299c1939
CSP_Evelyn-Chennault <evelyn.chennault@ucas-edu.net>
2025-03-03 11:25:41 -0700

    save

---
+++ Loops/Notes.py
@@ -0,0 +1,36 @@
+#Evelyn Chennault, Loops Notes Python
+
+#1. What is a loop? 
+    #Section of code that repeats multiple times.
+
+
+#2. What are the two types of loops?
+    #for loop
+nums = [12,3,66,7,3,3,2]
+
+for num in nums:
+    print(num)
+x = 0
+
+    #while loop
+while x < 10:
+    print(x)
+    x+= 1
+
+
+#3. What is iteration?
+    #Particular instance/time of the loop
+
+
+#4. What are lists? 
+
+
+#5. How do you make lists in Python? 
+
+
+#6. How do you make for loops in Python? 
+
+
+#7. How do you make while loops in Python?
+
+

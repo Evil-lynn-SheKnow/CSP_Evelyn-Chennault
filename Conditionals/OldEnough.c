f1982a43336cc6d9d6cd158e2c2219b39e6a5173
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-03-04 17:21:11 +0000

    save

--- Conditionals/OldEnough.c
+++ Conditionals/OldEnough.c
@@ -7,39 +7,13 @@ int main(void){
     printf("This prgram will tell you if you can vote, get a permit, dirve, and go to school.");
     printf("Please type your age here:\n");
     scanf("%d", &num);
-    //Can you vote?
-    if(num <= 5){
-        printf("No");
-    }else if(num <= 6 && num >= 17){
-        printf("No");
+    if(num <= 5 && num >= 11){
+        printf("You can not vote, get a learners permit, or drive, but you can go to school.");
+    }else if(num >= 12 && num <= 14){
+        printf("You can not vote or drive, but you can get a learners permit and go to school.");
+    }else if (num >= 15 && num <= 17){
+        printf("You can not vote, but you can get a learners permit, drive and go to school.");
     }else if(num >= 18){
-        printf("Yes");
+        printf("You can  vote, get a learners permit, drive and go to school.");
     }
-    //Can you get a learner's permit?
-    if(num <= 14){
-        printf("No");
-    }else if(num <= 15 && num >= 17){
-        printf("Yes");
-    }else if(num >= 18){
-        printf("Yes");
-    }
-    //Can you drive?
-    if(num <= 14){
-        printf("No");
-    }else if(num <= 15 && num >= 17){
-        printf("Yes");
-    }else if(num >= 18){
-        printf("Yes");
-    }
-
-    //Can you attend school?
-    if(num <= 5){
-        printf("No");
-    }else if(num <= 6 && num >= 17){
-        printf("Yes");
-    }else if(num >= 18){
-        printf("Yes");
-    }
-    
-        return 0;
-    }
+}

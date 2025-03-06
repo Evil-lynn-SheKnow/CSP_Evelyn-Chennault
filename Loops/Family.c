b94db9c3b9f382e47e0d5b23f79fad9da6b02a31
CSP_Evelyn-Chennault <evelyn.chennault@ucas-edu.net>
2025-03-06 11:23:05 -0700

    save

---
+++ Loops/Family.c
@@ -0,0 +1,10 @@
+//Evelyn Chennault, Family Loop
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void){
+    char family[] = {"Russell", "Kristin", "Violet", "Steven"};
+    printf("Hello World");
+    return 0;
+}

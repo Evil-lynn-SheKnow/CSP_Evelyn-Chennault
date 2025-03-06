a4a181d992de512fec3edf714cc17c0336a5b035
CSP_Evelyn-Chennault <evelyn.chennault@ucas-edu.net>
2025-03-06 11:34:42 -0700

    dave

--- Loops/Family.c
+++ Loops/Family.c
@@ -1,10 +1,14 @@
-//Evelyn Chennault, Family Loop
+//Evelyn Chennault, Family Loop 
 #include <stdio.h>
 #include <string.h>
 
 
 int main(void){
-    char family[] = {"Russell", "Kristin", "Violet", "Steven"};
-    printf("Hello World");
+    char family[][50] = {"Russell", "Kristin", "Violet", "Steven"};
+    int mlength = sizeof(family)/sizeof(family[0]);
+    int i;
+    for(i = 0;i < mlength; i++){
+        printf("Hello, %s\n", family[i]);
+    }
     return 0;
 }

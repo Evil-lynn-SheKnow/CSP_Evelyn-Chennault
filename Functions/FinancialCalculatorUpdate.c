e424d86410cfd05ce8172b5b9f077195bd75bc62
CSP_Evelyn-Chennault <evelyn.chennault@ucas-edu.net>
2025-03-19 16:20:49 -0600

    save

--- Functions/FinancialCalculatorUpdate.c
+++ Functions/FinancialCalculatorUpdate.c
@@ -12,23 +12,23 @@ float utilitiesPercent;
 float groceriesPercent;
 float transportationPercent;
 
-float question(char subject){
+float question(char subject[50]){
     float amount;
-    printf("What is your", subject);
+    printf("What is your %s\n", subject);
     scanf("%f", &amount);
     return amount;
 }
 
-void calculations(float amount, float income, char subject){
+void calculations(float amount, float income, char subject[50]){
     int percent = amount/income*100;
-    printf("Your %s is $%.2f, which is %d percent of your income.", subject, amount, percent);
+    printf("Your %s is $%.2f, which is %d percent of your income.\n", subject, amount, percent);
 }
 
 int main(void){
 
     //tells user what the program does
-    printf("Hello! I am a financial calculator. Let's begin! ");
-    printf("I will ask you several questions. Please answer them in terms of US dollars. ");
+    printf("Hello! I am a financial calculator. Let's begin! \n");
+    printf("I will ask you several questions. Please answer them in terms of US dollars. \n");
 
     //user will fill out in order for the calculator to get the variables
     income = question("income?");
@@ -37,20 +37,17 @@ int main(void){
     groceries = question("groceries?");
     transportation = question("transportation?");
 
-    float incomePercent = income*.1;
 
     calculations(rent, income, "income");
     calculations(utilities, income, "income");
     calculations(groceries, income, "income");
     calculations(transportation, income, "income");
     
-    float incomePercent = income*.1;
-    float spendings = income-rent-utilities-groceries-transportation-incomePercent;
+    float savings = income*.1;
+    float spendings = income-rent-utilities-groceries-transportation-savings;
 
-
-    printf("%f is 10 percent of your income.", incomePercent);
-    printf("Your savings are 10 percent of your income is $%f.", incomePercent);
-    printf("You have $%f left.", spendings);
+    printf("Your savings are 10 percent of your income is $%f.\n", savings);
+    printf("You have $%f left.\n", spendings);
 
     //ending
     printf("Thank you and have a good day! I hope we meet again.");

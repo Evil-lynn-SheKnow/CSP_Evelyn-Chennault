2e294a86ec1ee2910e79a255f1184748d4461b05
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-03-19 05:39:49 +0000

    save final changes to old enough in c

--- Conditionals/OldEnough.c
+++ Conditionals/OldEnough.c
@@ -7,13 +7,15 @@ int main(void){
     printf("This prgram will tell you if you can vote, get a permit, dirve, and go to school.");
     printf("Please type your age here:\n");
     scanf("%d", &num);
-    if(num <= 5 && num >= 11){
+    if(num >= 5 && num >= 11){
         printf("You can not vote, get a learners permit, or drive, but you can go to school.");
     }else if(num >= 12 && num <= 14){
         printf("You can not vote or drive, but you can get a learners permit and go to school.");
     }else if (num >= 15 && num <= 17){
         printf("You can not vote, but you can get a learners permit, drive and go to school.");
     }else if(num >= 18){
-        printf("You can  vote, get a learners permit, drive and go to school.");
+        printf("You can vote, get a learners permit, drive and go to school.");
+    }else if(num <= 4){
+        printf("You can't vote, get a learners permit, drive or go to school.");
     }
 }

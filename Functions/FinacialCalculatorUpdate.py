ef36cdea260bb604dae901bc57ecb916a093e169
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-03-27 03:01:40 +0000

    save

--- Functions/FinacialCalculatorUpdate.py
+++ Functions/FinacialCalculatorUpdate.py
@@ -4,29 +4,28 @@
 print("Hello, user. I am a finacial calculator. I will calculate your spendings and savings. Let's begin!\n")
 
 # user inputs
-income = float(input("What is your income?\n"))
+def info(cost, income, type):
+    percent= cost/income *100
+    print(f"your {type} is ${cost:.2f} rent which is,{percent}%, of your income")
 
-def info(type):
-    return input(f"How much do you have to pay for {type}:\n")
+def user(type):
+    question = "what is your monthly " + type + "?\n"
+    return float(input(question))
 
-rent = info("rent")
-utilites = info("utilites")
-groceries = info("groceries")
-transportation = info("transportation")
+income = user("income")
+rent = user("rent")
+utilities= user("utilities")
+groceries= user("groceries")
+transportation= user("transportation")
+spending= rent+utilities+groceries+transportation
+savings = income*0.1
 
-rent_per = round(rent/income*100)
-utilites_per = utilites/income*100
-groceries_per = groceries/income*100
-transport_per = transportation/income*100
-savings = income*.1
-spendings = income-rent-utilites-groceries-transportation-savings
-
-print(f"Your is ${income}.")
-print(f"You spend ${rent} for your rent, which is {rent_per}% of your income.")
-print(f"You spend ${utilites} for your utilites, which is {utilites_per}% of your income.")
-print(f"You spend ${groceries} for your groceries, which is {groceries_per}% of your income.")
-print(f"You spend ${transportation} for transportation, which is {transport_per}% of your income.")
-print(f"You spend ${spendings} for all purposes,.")
-print(f"${savings} is 10% of your income.")
+info(rent, income, "rent")
+info(utilities, income, "utilites")
+info(groceries, income, "groceries")
+info(transportation, income, "transportation")
+info(rent, income, "rent")
+info(savings, income, "savings")
+info(spending,income, "spending")
 
 print("Thank you for trying me out! I hope I helped with your finacial needs.")

51543afb261e4ca78f98e8be37f979aa9fafc943
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-04-22 11:21:46 -0600

    save

--- JavaScript/notes.js
+++ JavaScript/notes.js
@@ -2,5 +2,5 @@ function hello(){
     document.getElementById("title").innerHTML = "Hello World"
 }
 function hover(){
-    document.getElementById("img").src = ""
+    document.getElementById("image").src = "https://media.ed.edmunds-media.com/dodge/challenger/2023/oem/2023_dodge_challenger_coupe_gt_fq_oem_1_1600.jpg"
 }

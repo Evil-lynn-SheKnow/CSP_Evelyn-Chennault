56b45686ad879039305d60da879596af63b8fc9c
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-04-22 11:33:22 -0600

    save

--- JavaScript/notes.js
+++ JavaScript/notes.js
@@ -1,4 +1,14 @@
 let images = ["https://media.ed.edmunds-media.com/dodge/challenger/2023/oem/2023_dodge_challenger_coupe_gt_fq_oem_1_1600.jpg", "https://www.supercars.net/blog/wp-content/uploads/2016/03/2009_Chevrolet_CamaroSS2.jpg"]
+let counter = 0
+
+function change(){
+    if (counter <= images.length){
+        document.getElementById("image").src = images[counter]
+}else{
+    counter = 0
+    document.getElementById("image").src = images[counter]
+}
+}
 
 function hello(){
     document.getElementById("title").innerHTML = "Hello World"

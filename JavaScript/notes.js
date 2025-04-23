6b9c9f2a1b34b30585d47b3afe92107e4ebea235
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-04-23 11:37:23 -0600

    save

--- JavaScript/notes.js
+++ JavaScript/notes.js
@@ -17,18 +17,19 @@
   }
   
   function hello(){
-      document.getElementById("title").innerHTML = "Hello World!"
+    let name = window.prompt("What is your name?")
+    document.getElementById("title").innerHTML = "Hello, " + name + "!"
   }
   
   function hover(){
-      document.getElementById("img").src = "https://media.ed.edmunds-media.com/dodge/challenger/2023/oem/2023_dodge_challenger_coupe_gt_fq_oem_1_1600.jpg"
+    document.getElementById("img").src = "https://media.ed.edmunds-media.com/dodge/challenger/2023/oem/2023_dodge_challenger_coupe_gt_fq_oem_1_1600.jpg"
   }
   
   function leave(){
-      document.getElementById("img").src = "https://www.supercars.net/blog/wp-content/uploads/2016/03/2009_Chevrolet_CamaroSS2.jpg"
+    document.getElementById("img").src = "https://www.supercars.net/blog/wp-content/uploads/2016/03/2009_Chevrolet_CamaroSS2.jpg"
   }
   
   function hidden(){
-      document.getElementById("meme").style.display = "block"
+    document.getElementById("meme").style.display = "block"
   }
   //You cannot unhide this by calling it on the image. You have to attach it somewhere else.

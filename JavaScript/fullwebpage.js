53ba59b64a61274f55ac47f35ab9e5dce61b5161
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-04-23 11:38:56 -0600

    save new docs

---
+++ JavaScript/fullwebpage.js
@@ -0,0 +1,35 @@
+//function hello(){
+    //  document.getElementById("title").style.color = "orange"
+  //}
+  let images = ["https://media.ed.edmunds-media.com/dodge/challenger/2023/oem/2023_dodge_challenger_coupe_gt_fq_oem_1_1600.jpg", "https://www.supercars.net/blog/wp-content/uploads/2016/03/2009_Chevrolet_CamaroSS2.jpg"]
+  
+  let counter = 0
+  
+  function change(){
+      if (counter <= images.length){
+          document.getElementById("img").src=images[counter] 
+          counter +=1
+      }
+      else{
+          counter=0
+          document.getElementById("img").src=images[counter]
+      }
+  }
+  
+  function hello(){
+    let name = window.prompt("What is your name?")
+    document.getElementById("title").innerHTML = "Hello, " + name + "!"
+  }
+  
+  function hover(){
+    document.getElementById("img").src = "https://media.ed.edmunds-media.com/dodge/challenger/2023/oem/2023_dodge_challenger_coupe_gt_fq_oem_1_1600.jpg"
+  }
+  
+  function leave(){
+    document.getElementById("img").src = "https://www.supercars.net/blog/wp-content/uploads/2016/03/2009_Chevrolet_CamaroSS2.jpg"
+  }
+  
+  function hidden(){
+    document.getElementById("meme").style.display = "block"
+  }
+  //You cannot unhide this by calling it on the image. You have to attach it somewhere else.

1abc11c92be1174d2ecb401604c84798d02e8d37
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-04-28 11:15:46 -0600

    save

---
+++ Internet Final/staycation.js
@@ -0,0 +1,14 @@
+function hidden(){
+    document.getElementById("more").style.display = "block"
+}
+
+function view(){
+    if(document.getElementById("more").style.display == "block"){
+      document.getElementById("more").style.display = "none"
+      document.getElementById("shw").innerHTML = "Show More"
+    }else{
+      document.getElementById("more").style.display = "block"
+      document.getElementById("shw").innerHTML = "Show Less"
+    }
+  
+  }

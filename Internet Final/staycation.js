166c1167a5498d83c7cd51f7160b08a7907e9912
Evil-lynn-SheKnow <evelyn.chennault@ucas-edu.net>
2025-05-01 13:16:52 -0600

    save

--- Internet Final/staycation.js
+++ Internet Final/staycation.js
@@ -1,8 +1,10 @@
-let images = ["https://www.nps.gov/common/uploads/grid_builder/zion/crop16_9/96F99947-E5A2-F6CD-7FCDAC403E009B18.jpeg?width=1300&quality=90&mode=crop", "https://cdn.shedefined.com.au/wp-content/uploads/2024/07/24142251/Travel-guide-to-Zion-National-Park-Utah-USA-HERO-960x540-1.jpg", "https://www.mceconferences.com/wp-content/uploads/Zion-ss.jpg"]
+let one = ["https://roadtripqueens.blog/wp-content/uploads/2024/04/3081f76d-dc5c-4785-a554-384c0a2e34f4-1.jpg?w=1024", "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/32/ce/b0/western-hills.jpg?w=500&h=500&s=1", "https://www.bons-plans-voyage-ouest-americain.com/wordpress/wp-content/uploads/2024/02/belly-of-the-dragon-9-800x500.jpg"]
+
+let two = ["https://i0.wp.com/soulsitemedia.com/wp-content/uploads/2019/11/kanabb-0711.jpg?fit=750%2C500&ssl=1", "https://spacetourismguide.com/wp-content/uploads/2019/05/Zion-National-Park-Kolob-Canyon-1500x997.jpg", "https://zionapp.s3.us-west-1.amazonaws.com/highlightItemImages/61e5ec077d20d262000509.jpg"]
   
 let counter = 0
     
-function change(){
+function rotate(){
     if (counter <= images.length){
         document.getElementById("img").src=images[counter] 
         counter +=1
